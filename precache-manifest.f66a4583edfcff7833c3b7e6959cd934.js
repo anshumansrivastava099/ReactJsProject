62ba077499323279077e7a301b078f94713ba32d
Anshuman <anshumansrivastava099@gmail.com>
2020-07-17 19:21:58 +0530

    Updates

---
+++ precache-manifest.f66a4583edfcff7833c3b7e6959cd934.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6393ce7829c236a44e7",
+    "url": "/ReactJsProject/static/css/main.76c88329.chunk.css"
+  },
+  {
+    "revision": "a6393ce7829c236a44e7",
+    "url": "/ReactJsProject/static/js/main.f79345f7.chunk.js"
+  },
+  {
+    "revision": "0f0dec3ce332140114a7",
+    "url": "/ReactJsProject/static/js/runtime~main.0b27acbc.js"
+  },
+  {
+    "revision": "116604a55f448af1f1c9",
+    "url": "/ReactJsProject/static/css/2.70c4ce66.chunk.css"
+  },
+  {
+    "revision": "116604a55f448af1f1c9",
+    "url": "/ReactJsProject/static/js/2.2d881cda.chunk.js"
+  },
+  {
+    "revision": "1fc0559a938101bbca4c8e82dcfa8bf2",
+    "url": "/ReactJsProject/static/media/home.1fc0559a.svg"
+  },
+  {
+    "revision": "19981cc0ad2ba2d89fb41e2fddb8b3fe",
+    "url": "/ReactJsProject/static/media/s1.19981cc0.jpg"
+  },
+  {
+    "revision": "083d362d078f0920460cd5909c79bc3e",
+    "url": "/ReactJsProject/index.html"
+  }
+];
